2042a59d7afa25928d1fb5712381987c46df381f
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-21 11:08:49 +0100

    project structurev0.1

--- YouDo/app/src/main/java/com/example/youdo/ToDo.java
+++ YouDo/app/src/main/java/com/example/youdo/ToDo.java
@@ -7,21 +7,102 @@ import androidx.room.ForeignKey;
 import androidx.room.PrimaryKey;
 
 @Entity(tableName = "todo_table",
-        foreignKeys = @ForeignKey(entity = Type.class,
-                parentColumns = "typeId",
-                childColumns = "typeId",
-                onDelete = ForeignKey.CASCADE))
-
+        foreignKeys = {
+                @ForeignKey(entity = Type.class,
+                        parentColumns = "typeId",
+                        childColumns = "typeId",
+                        onDelete = ForeignKey.CASCADE),
+                @ForeignKey(entity = User.class,
+                        parentColumns = "userId",
+                        childColumns = "userId",
+                        onDelete = ForeignKey.CASCADE)
+        })
 public class ToDo {
     @PrimaryKey(autoGenerate = true)
     private int todoId;
+
     @NonNull
     private String name;
-    private String date;
-    private String time;
-    private String description;
-    private int typeId;
-    private String state;
-    // functions
-}
 
+    private String date;
+
+    private String time;
+
+    private String description;
+
+    private int typeId;
+
+    private int userId;
+
+    private String state;
+
+    // functions
+
+    public ToDo() {
+    }
+
+    public int getTodoId() {
+        return todoId;
+    }
+
+    public void setTodoId(int todoId) {
+        this.todoId = todoId;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
--- YouDo/app/src/main/java/com/example/youdo/Type.java
+++ YouDo/app/src/main/java/com/example/youdo/Type.java
@@ -11,5 +11,35 @@ public class Type {
     @NonNull
     private String name;
     private String colour;
+
     // functions
+
+    public Type(@NonNull String name, String colour) {
+        this.name = name;
+        this.colour = colour;
+    }
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(int typeId) {
+        this.typeId = typeId;
+    }
+
+    @NonNull
+    public String getName() {
+        return name;
+    }
+
+    public void setName(@NonNull String name) {
+        this.name = name;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 }
