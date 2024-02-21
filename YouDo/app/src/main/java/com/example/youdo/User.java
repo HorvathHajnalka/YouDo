290651f60186f18d129fee513feda76bfc9e7c55
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-21 11:08:49 +0100

    project structurev0.1

--- YouDo/app/src/main/java/com/example/youdo/User.java
+++ YouDo/app/src/main/java/com/example/youdo/User.java
@@ -17,4 +17,45 @@ public class User {
     private String password;
 
     // functions
+    public User(@NonNull String userName, @NonNull String email, @NonNull String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    @NonNull
+    public String getUserName() {
+        return userName;
+    }
+
+    @NonNull
+    public String getEmail() {
+        return email;
+    }
+
+    @NonNull
+    public String getPassword() {
+        return password;
+    }
+
+    // Setterek
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setUserName(@NonNull String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(@NonNull String email) {
+        this.email = email;
+    }
+
+    public void setPassword(@NonNull String password) {
+        this.password = password;
+    }
 }
