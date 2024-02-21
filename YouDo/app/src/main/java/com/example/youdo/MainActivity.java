5a66a88acc2936bdbe27ff98fa0ecaf5738cff24
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-21 17:46:30 +0100

    before dbconnect

--- YouDo/app/src/main/java/com/example/youdo/MainActivity.java
+++ YouDo/app/src/main/java/com/example/youdo/MainActivity.java
@@ -6,6 +6,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
     }
 }
 
---
+++ YouDo/app/src/main/java/com/example/youdo/RegisterActivity.java
@@ -0,0 +1,27 @@
+package com.example.youdo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView newaccountbtn  = (TextView) findViewById(R.id.alreadyHaveAccount);
+
+        // switch to login page
+        newaccountbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+    }
+}
