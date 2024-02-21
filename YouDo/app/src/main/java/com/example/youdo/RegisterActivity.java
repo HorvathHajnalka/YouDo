bbca9e0cd0832c73064e904b43dddf8a6a4c7606
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-21 19:35:53 +0100

    dbconnect-v1.0

--- YouDo/app/src/main/java/com/example/youdo/RegisterActivity.java
+++ YouDo/app/src/main/java/com/example/youdo/RegisterActivity.java
@@ -5,22 +5,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText emailReg, userNameReg, passwordReg, password2Reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        emailReg = findViewById(R.id.email);
+        userNameReg = findViewById(R.id.username);
+        passwordReg = findViewById(R.id.password);
+        password2Reg = findViewById(R.id.password2);
+
+        MaterialButton regbtn  = (MaterialButton) findViewById(R.id.regbtn);
         TextView newaccountbtn  = (TextView) findViewById(R.id.alreadyHaveAccount);
+
 
         // switch to login page
         newaccountbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+            }
+        });
+
+
+        regbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String strEmail = emailReg.getText().toString();
+                String strUserName = userNameReg.getText().toString();
+                String strPassword = passwordReg.getText().toString();
+                String strPassword2 = password2Reg.getText().toString();
+
+                if(strUserName.isEmpty() || strEmail.isEmpty() || strPassword.isEmpty() || strPassword2.isEmpty()){
+                    Toast.makeText(RegisterActivity.this, "Empty fields!", Toast.LENGTH_SHORT).show();
+                } else if (strPassword != strPassword2) {
+                    Toast.makeText(RegisterActivity.this, "Passwords not matching!", Toast.LENGTH_SHORT).show();
+                } else {
+
+                }
             }
         });
     }
--- YouDo/app/src/main/java/com/example/youdo/User.java
+++ YouDo/app/src/main/java/com/example/youdo/User.java
@@ -2,18 +2,13 @@ package com.example.youdo;
 
 
 import androidx.annotation.NonNull;
-import androidx.room.Entity;
-import androidx.room.PrimaryKey;
 
-@Entity(tableName = "user_table")
+
+
 public class User {
-    @PrimaryKey(autoGenerate = true)
     private int userId;
-    @NonNull
     private String userName;
-    @NonNull
     private String email;
-    @NonNull
     private String password;
 
     // functions
