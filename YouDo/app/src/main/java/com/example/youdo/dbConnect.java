a3b3018df2f5ecd37796469b66de07043e9d1de2
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-22 18:35:24 +0100

    dbconnect-v2.0

--- YouDo/app/src/main/java/com/example/youdo/dbConnect.java
+++ YouDo/app/src/main/java/com/example/youdo/dbConnect.java
@@ -1,118 +1,3 @@
-/*package com.example.youdo;
-
-import android.content.ContentValues;
-import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
-
-import androidx.annotation.Nullable;
-
-public class dbConnect extends SQLiteOpenHelper {
-
-    private static String userTable = "users";
-    private String password = "password";
-    private static String userId = "userId";
-    private String userName = "userName";
-    private String email = "email";
-    // db parameters
-    private static String dbName = "youDo_DB";
-    private static int dbVersion = 1;
-    public dbConnect(@Nullable Context context) {
-        super(context, dbName, null, dbVersion);
-    }
-
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + userTable + "(" + userId + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + userName + " TEXT, "+ email + " TEXT, "+password+ " TEXT)";
-
-        db.execSQL(query);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+ userTable);
-        onCreate(db);
-    }
-
-    public boolean checkEmail(String emailcheck){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String query = " SELECT * FROM "+ userTable + " WHERE " + email + " =?";
-        Cursor cursor = db.rawQuery(query, new String[]{emailcheck});
-        if (cursor.moveToFirst()){
-            // email has already registered
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public boolean checkName(String namecheck){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String query = " SELECT * FROM "+ userTable + " WHERE " + userName + " =?";
-        Cursor cursor = db.rawQuery(query, new String[]{namecheck});
-        if (cursor.moveToFirst()){
-            // username has already registered
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public void addUser(User user){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-
-        values.put(userName, user.getUserName());
-        values.put(email, user.getEmail());
-        values.put(password, user.getPassword());
-
-        db.insert(userTable,null,values);
-    }
-
-    public boolean updateUser(User user){
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues values = new ContentValues();
-
-        values.put(userName, user.getUserName());
-        values.put(email, user.getEmail());
-        values.put(password, user.getPassword());
-
-        int endResult = db.update(userTable, values, userId = " =?", new String[]{String.valueOf(user.getUserId())});
-        if (endResult > 0){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public boolean deleteUser(User user){
-        SQLiteDatabase db = this.getWritableDatabase();
-
-        int endResult = db.delete(userTable,userId = " =?", new String[]{String.valueOf(user.getUserId())});
-        if (endResult > 0){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-    public boolean checkUser(String userNameLogIn, String passwordLogIn){
-        SQLiteDatabase db = this.getReadableDatabase();
-        String query = " SELECT * FROM "+ userTable + " WHERE " + userName + " =? AND " + password + " =?";
-        Cursor cursor = db.rawQuery(query,new String[]{userNameLogIn, passwordLogIn});
-        if (cursor.moveToFirst()){
-            return true;
-        }else{
-            return false;
-        }
-    }
-
-
-}*/
-
 package com.example.youdo;
 
 import android.content.Context;
@@ -123,13 +8,36 @@ import androidx.annotation.Nullable;
 
 public class dbConnect extends SQLiteOpenHelper {
     // db parameters
+
+    // database
     private static String dbName = "youDo_DB";
     private static int dbVersion = 1;
-    public static String userTable = "users";
+
+    // users
+    public static String userTable = "user";
     public static String userId = "userId";
     public static String userName = "userName";
     public static String email = "email";
     public static String password = "password";
+
+    // todos
+    public static String todoTable = "todo";
+    public static String todoId = "todoId";
+    public static String todoName = "todoName";
+    public static String todoDesc = "todoDesc";
+    public static String todoState = "todoState"; // Boolean?
+    public static String todoDate = "todoDate"; // Date?
+    public static String todoTime = "todoTime"; // Time?
+    // ?
+    public static String todoTypeId = "todoTypeId"; // foreign key
+    public static String todoUserId = "todoUserId"; // foreign key
+
+    // type
+    public static String typeTable = "type";
+    public static String typeId = "typeId";
+    public static String typeName = "typeName";
+    public static String typeColour = "typeColour";
+
 
     public dbConnect(@Nullable Context context) {
         super(context, dbName, null, dbVersion);
@@ -137,14 +45,28 @@ public class dbConnect extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String query = "CREATE TABLE " + userTable + "(" + userId + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+
+        // user table
+        String makeUserQuery = "CREATE TABLE " + userTable + "(" + userId + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + userName + " TEXT, "+ email + " TEXT, "+ password + " TEXT)";
-        db.execSQL(query);
+        db.execSQL(makeUserQuery);
+
+        // todo table
+        String makeTodoQuery = "CREATE TABLE " + todoTable + "(" + todoId + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + todoName + " TEXT, "+ todoDesc + " TEXT, "+ todoState + " TEXT, " + todoDate + " TEXT, "+ todoTime + " TEXT)";
+        db.execSQL(makeTodoQuery);
+
+        // type table
+        String makeTypeQuery = "CREATE TABLE " + typeTable + "(" + typeId + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + typeName + " TEXT, "+ typeColour + " TEXT)";
+        db.execSQL(makeTypeQuery);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+ userTable);
+        db.execSQL("DROP TABLE IF EXISTS "+ todoTable);
+        db.execSQL("DROP TABLE IF EXISTS "+ typeTable);
         onCreate(db);
     }
 }
--- YouDo/app/src/main/java/com/example/youdo/dbConnectToDo.java
+++ YouDo/app/src/main/java/com/example/youdo/dbConnectToDo.java
@@ -1,4 +1,48 @@
 package com.example.youdo;
 
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
 public class dbConnectToDo {
+    private dbConnect dbHelper;
+
+    public dbConnectToDo(Context context) {
+        dbHelper = new dbConnect(context);
+    }
+
+    public void addToDo(ToDo todo){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(dbConnect.todoName, todo.getName());
+        values.put(dbConnect.todoDesc, todo.getDescription());
+        values.put(dbConnect.todoState, todo.getState());
+        values.put(dbConnect.todoState, todo.getDate());
+        values.put(dbConnect.todoState, todo.getTime());
+
+        db.insert(dbConnect.todoTable, null, values);
+    }
+
+    public boolean updateToDo(ToDo todo){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        values.put(dbConnect.todoName, todo.getName());
+        values.put(dbConnect.todoDesc, todo.getDescription());
+        values.put(dbConnect.todoState, todo.getState());
+        values.put(dbConnect.todoState, todo.getDate());
+        values.put(dbConnect.todoState, todo.getTime());
+
+        int endResult = db.update(dbConnect.todoTable, values, dbConnect.todoId + " =?", new String[]{String.valueOf(todo.getTodoId())});
+        return endResult > 0;
+    }
+
+    public boolean deleteToDo(ToDo todo){
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        int endResult = db.delete(dbConnect.todoTable, dbConnect.todoId + " =?", new String[]{String.valueOf(todo.getUserId())});
+        return endResult > 0;
+    }
+
+
 }
