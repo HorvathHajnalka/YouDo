65c1932f5a95cf33ee97cf1b0ac55761c1379470
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-22 21:17:47 +0100

    todo-main-v1.0

--- YouDo/app/build.gradle.kts
+++ YouDo/app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     // LiveData
     implementation("androidx.lifecycle:lifecycle-livedata:2.7.0")
 
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.11.0")
+
 }
