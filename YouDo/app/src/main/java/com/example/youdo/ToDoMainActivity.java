c5c4c2668ffcd7e8398f34355c3201d2f84cad63
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-23 01:06:39 +0100

    todo-main-v2.0

--- YouDo/app/src/main/java/com/example/youdo/ToDoMainActivity.java
+++ YouDo/app/src/main/java/com/example/youdo/ToDoMainActivity.java
@@ -2,13 +2,29 @@ package com.example.youdo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ToDoMainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_main);
+
+        fab = findViewById(R.id.addToDobtn);
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(ToDoMainActivity.this, UploadToDoActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
