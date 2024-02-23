a367d60bf151635b8ac0df14d83a8ed0530679d3
HorvathHajnalka <h.hajni2003@gmail.com>
2024-02-23 10:12:47 +0100

    Revert "revert-2024-02-03-10-12"
    This reverts commit 8aba0d304f38eec1c1ed4c01f2c4c0fcd38ae937.

--- YouDo/build.gradle.kts
+++ YouDo/build.gradle.kts
@@ -1,11 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-
-buildscript {
-    repositories{
-        google()
-        mavenCentral()
-    }
-}
 plugins {
     id("com.android.application") version "8.2.2" apply false
 }
