bc0776ff6ffa9cf876fef6fa8a4f4c3e2ed4a7ed
Horvath Hajnalka <h.hajni2003@gmail.com>
2025-03-06 21:42:08 +0100

    update-detail-activity

--- YouDo/app/src/main/java/com/example/youdo/ToDoDetailActivity.java
+++ YouDo/app/src/main/java/com/example/youdo/ToDoDetailActivity.java
@@ -1,12 +1,16 @@
 package com.example.youdo;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -23,9 +27,10 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class ToDoDetailActivity extends AppCompatActivity {
 
-    TextView detailTitle, detailDesc, detailDate;
+    TextView detailTitle, detailDesc, detailDate, targetTimeText, achievedTimeText, typeText;
     MaterialButton editTodoBtn, delTodoBtn, doneTodoBtn;
     dbConnectToDo dbConnectToDo;
+    dbConnectToDoType dbConnectToDoType;
     ToDo todo;
     int todoId = -1; // has to be updated
     int userId = -1; // has to be updated
@@ -42,6 +47,9 @@ public class ToDoDetailActivity extends AppCompatActivity {
         delTodoBtn = findViewById(R.id.delTodoBtn);
         doneTodoBtn = findViewById(R.id.doneTodoBtn);
         detailDate = findViewById(R.id.detailDate);
+        targetTimeText = findViewById(R.id.targetTimeText);
+        achievedTimeText = findViewById(R.id.achievedTimeText);
+        typeText= findViewById(R.id.typeText);
 
 
         Intent intent = getIntent();
@@ -52,6 +60,8 @@ public class ToDoDetailActivity extends AppCompatActivity {
         }
 
         dbConnectToDo = new dbConnectToDo(this);
+        dbConnectToDoType = new dbConnectToDoType(this);
+
         todo = dbConnectToDo.getTodoById(todoId);
 
         // send UI the to-do details if to-do exists
@@ -64,6 +74,14 @@ public class ToDoDetailActivity extends AppCompatActivity {
                 detailDesc.setBackgroundColor(ContextCompat.getColor(ToDoDetailActivity.this, R.color.blue_extra_dark));
             }
             detailDate.setText("Date: " + todo.getDate());
+            targetTimeText.setText("Target Time: " + todo.getTargetMinutes() + " min.");
+            achievedTimeText.setText("Achieved Time: " + todo.getAchievedMinutes() + " min.");
+            Type curr_type = dbConnectToDoType.getToDoTypeById(todo.getTypeId());
+            if(curr_type!= null){
+                typeText.setText("Activity Type: " + curr_type.getName());
+            }else{
+                typeText.setText("Activity Type: -");
+            }
             if (todo.isDone()) doneTodoBtn.setText("ToDo is Not Done!");
 
         } else {
@@ -74,19 +92,57 @@ public class ToDoDetailActivity extends AppCompatActivity {
         doneTodoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(!todo.isDone()) todo.setDone(true);
-                else todo.setDone(false);
+                if (todo.getTargetMinutes() > 0 && !todo.isDone()) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(ToDoDetailActivity.this);
+                    builder.setTitle("Completion Time");
 
+                    // EditText
+                    final EditText input = new EditText(ToDoDetailActivity.this);
+                    input.setInputType(InputType.TYPE_CLASS_NUMBER);
+                    input.setHint("Enter achieved time (minutes)");
+                    builder.setView(input);
 
+                    // Buttons
+                    builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            String enteredTime = input.getText().toString().trim();
+                            if (!enteredTime.isEmpty()) {
+                                int achievedTime = Integer.parseInt(enteredTime);
+                                todo.setAchievedMinutes(achievedTime);
+                            } else {
+                                todo.setAchievedMinutes(0);
+                            }
+
+                            toggleTodoStatus();
+                        }
+                    });
+
+                    builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+
+                    builder.show();
+                } else {
+                    todo.setAchievedMinutes(0);
+                    toggleTodoStatus();
+                }
+            }
+
+            private void toggleTodoStatus() {
+                todo.setDone(!todo.isDone());
                 dbConnectToDo.updateToDo(todo);
 
                 Intent i = new Intent(ToDoDetailActivity.this, ToDoMainActivity.class);
                 i.putExtra("userId", userId);
                 i.putExtra("curr_date", curr_date);
                 startActivity(i);
-
             }
         });
+
 
         editTodoBtn.setOnClickListener(new View.OnClickListener() {
             @Override
